b47e9a40b24b192c2b52622cc9e73f40d397ee78
rndexe <himanshu.ernde@gmail.com>
2025-01-29 14:41:40 +0530

    Add vite plugin terminal

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Terminal from 'vite-plugin-terminal';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: process.env.BASE_PATH ?? '',
-    plugins: [react()],
+    plugins: [
+        react(),
+        Terminal({
+            console: 'terminal',
+        }),
+    ],
     server: {
         host: true,
     },
