3c4ebf55ae0873fbbf659816eeb52f23f1157ceb
rndexe <himanshu.ernde@gmail.com>
2025-01-30 18:36:12 +0530

    remove terminal, update title

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import Terminal from 'vite-plugin-terminal';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: process.env.BASE_PATH ?? '',
-    plugins: [
-        react(),
-        Terminal({
-            console: 'terminal',
-        }),
-    ],
+    plugins: [react()],
     server: {
         host: true,
     },
