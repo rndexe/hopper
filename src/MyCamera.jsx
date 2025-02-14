e958167c114db913119eadb2653bbe64152a0997
rndexe <himanshu.ernde@gmail.com>
2025-02-14 23:39:19 +0530

    camera follow

--- src/MyCamera.jsx
+++ src/MyCamera.jsx
@@ -1,9 +1,18 @@
 import { PerspectiveCamera } from '@react-three/drei';
-import { R, theta } from './constants';
+import { useFrame } from '@react-three/fiber';
+import { mutation } from './utils';
+import { Vector3 } from 'three';
+import { groundLevel } from './constants';
 
+const target = new Vector3();
 export default function MyCamera() {
+    useFrame(({ camera }, delta) => {
+        camera.position.lerp({ x: 10 + mutation.position.x, y: 25, z: 50 + mutation.position.z }, 0.1);
+        target.copy({ x: mutation.position.x, y: groundLevel + 1, z: mutation.position.z });
+        camera.lookAt(target);
+    });
     return (
-        <PerspectiveCamera makeDefault fov={20} position={[10,25,50]}>
+        <PerspectiveCamera makeDefault fov={20} position={[10, 25, 50]}>
             <directionalLight position={[100, 100, 100]} intensity={2} />
         </PerspectiveCamera>
     );
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,2 @@
 export const groundLevel = 0.25;
-export const R = 20;
-export const theta = Math.PI / 24;
 export const boundarySize = 30
