518f8a4e69c201bf4bca7c0f769335a1bc646a5e
rndexe <himanshu.ernde@gmail.com>
2025-02-14 23:17:50 +0530

    box boundary

--- src/Ground.jsx
+++ src/Ground.jsx
@@ -1,12 +1,34 @@
 import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
-import { useTexture } from '@react-three/drei';
+import { useTexture, Box } from '@react-three/drei';
 import { CuboidCollider, RigidBody } from '@react-three/rapier';
-import { floorHeight } from './constants';
+import { boundarySize, groundLevel } from './constants';
 import { mutation } from './utils';
 // import { Grid } from '@react-three/drei';
 
 export default function Ground() {
+    return (
+        <>
+            <RigidBody type="fixed" userData={{ name: 'ground' }} restitution={0}>
+                <CuboidCollider args={[50, groundLevel, 50]} />
+            </RigidBody>
+            <GrassPlane />
+            <ShadowPlane />
+            <BoxBoundary />
+        </>
+    );
+}
+
+function GrassPlane() {
+    return (
+        <mesh rotation-x={-Math.PI / 2} scale={[10, 10, 1]} position-y={groundLevel - 0.2}>
+            <planeGeometry args={[10, 10]} />
+            <meshBasicMaterial color={'#4ade80'} />
+        </mesh>
+    );
+}
+
+function ShadowPlane() {
     const shadowTexture = useTexture('./shadow.jpg');
     const shadowRef = useRef();
     useFrame((state, delta) => {
@@ -14,21 +36,32 @@ export default function Ground() {
         shadowRef.current.position.z = mutation.position.z;
         shadowRef.current.material.opacity = 1 - Math.abs(mutation.position.y) * 0.15;
     });
-
     return (
-        <>
-            <RigidBody type="fixed" rotation={[-Math.PI / 2, 0, 0]} userData={{ name: 'ground' }} restitution={0}>
-                <CuboidCollider args={[50, 50, 0.25]} />
-            </RigidBody>
-            <mesh rotation={[-Math.PI / 2, 0, 0]} scale={[10, 10, 1]} position={[0, floorHeight / 2 - 0.2, 0]}>
-                <planeGeometry args={[10, 10]} />
-                <meshBasicMaterial color={'#4ade80'} />
-            </mesh>
-            {/* <Grid sectionSize={10} cellSize={1} args={[100, 100]} cellColor={'black'} position-y={floorHeight / 2} /> */}
-            <mesh rotation-x={-Math.PI / 2} position-y={floorHeight / 2 - 0.01} ref={shadowRef}>
-                <planeGeometry args={[5, 5]} />
-                <meshBasicMaterial color={'black'} transparent alphaMap={shadowTexture} opacity={0.5} />
-            </mesh>
-        </>
+        <mesh rotation-x={-Math.PI / 2} position-y={groundLevel - 0.01} ref={shadowRef} scale={4}>
+            <planeGeometry args={[1, 1]} />
+            <meshBasicMaterial color={'black'} transparent alphaMap={shadowTexture} opacity={1} />
+        </mesh>
+    );
+}
+
+function BoxBoundary() {
+    return (
+        <RigidBody type="fixed" restitution={1} colliders={false}>
+            {Array.from({ length: 12 }, (_, i) => {
+                const angle = (i / 12) * Math.PI * 2;
+                return (
+                    <CuboidCollider
+                        key={i}
+                        args={[1, 2, boundarySize * Math.tan(Math.PI / 12)]}
+                        position={[
+                            Math.cos(angle) * (boundarySize + 1),
+                            groundLevel + 2,
+                            Math.sin(angle) * (boundarySize + 1),
+                        ]}
+                        rotation={[0, -angle, 0]}
+                    />
+                );
+            })}
+        </RigidBody>
     );
 }
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
-export const floorHeight = 0.5;
+export const groundLevel = 0.25;
 export const R = 20;
 export const theta = Math.PI / 24;
+export const boundarySize = 30
