5219abebb19be84ded07a9238df61b21454aeb8e
rndexe <himanshu.ernde@gmail.com>
2025-02-24 21:33:40 +0530

    remove incorrect glb preload

--- src/models/SceneModel.jsx
+++ src/models/SceneModel.jsx
@@ -37,8 +37,6 @@ export function Model(props) {
     );
 }
 
-useGLTF.preload('/tmgtchi-transformed.glb');
-
 export default function Scenery() {
     return (
         <Instances castShadow receiveShadow>
