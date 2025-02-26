b943520f4a90e2bed9a499cc6cbf2c29e968d2d1
rndexe <himanshu.ernde@gmail.com>
2025-02-26 19:40:10 +0530

    only reduce health when page active

--- src/ui/Hud.jsx
+++ src/ui/Hud.jsx
@@ -5,7 +5,13 @@ export default function Hud() {
     const health = useGame((s) => s.health);
     const { changeHealth } = useGameActions();
 
-    useRandomInterval(() => changeHealth(-5), 10000, 12000);
+    useRandomInterval(
+        () => {
+            if (!document.hidden && document.hasFocus()) changeHealth(-5);
+        },
+        10000,
+        12000,
+    );
 
     return <div className="healthbar">Health : {health}</div>;
 }
